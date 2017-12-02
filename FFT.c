6e6e2ec849cd6ab67a865b14430c132c647900d7
jrzhu <cavi1520@sohu.com>
2017-12-02 07:53:09 -0800

    FFT for polynomy multiply available

--- FFT.c
+++ FFT.c
@@ -4,75 +4,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "timecount.h"
 
-typedef struct complex{	//val*exp{2*pi*i*power/n}
-	double val;
-	int power;
+typedef struct complex{	//real+im*i
+	double real;
+	double im;
 }complex;
+
 
 double A[64],B[64],C[128];
 complex ya[128],yb[128],yc[128];  
-complex aa[128],ab[128];
+complex aa[128],ab[128],ac[128];
 
-
-complex complexadd(complex a,complex b){
-	int i;
-	complex result;
-	for (i=0;i<64;++i){
-		//a.val[i]*b.val[i]
-	}
-	return result;
+void complexadd(complex *result,complex a,complex b){
+	(*result).real=a.real+b.real;
+	(*result).im=a.im+b.im;
 }
 
-complex complexmul(complex a,complex b){
-	complex result;
-	return result;
+void complexsub(complex *result,complex a,complex b){
+	(*result).real=a.real-b.real;
+	(*result).im=a.im-b.im;
+}
+
+void complexmul(complex *result,complex a,complex b){
+	(*result).real=a.real*b.real-a.im*b.im;
+	(*result).im=a.real*b.im+b.real*a.im;
 }
 
 void FFT(int N,complex a[],complex y[]){
-	if (N==1) return;
+	if (N==1) {
+		y[0].real=a[0].real;
+		y[0].im=a[0].im;
+		return;
+	}
 	//initialize
 	int i;
-	complex w;
-	w.val=1;
-	w.power=0;
+	complex w,wn,tmp;
+	w.real=1;
+	w.im=0;
+	wn.real=cos(2*M_PI/N);
+	wn.im=sin(2*M_PI/N);
+	
 	complex a0[N/2];
 	complex a1[N/2];
 	for (i=0;i<N/2;++i){
 		memcpy(&a0[i],&a[2*i],sizeof(complex));
 		memcpy(&a1[i],&a[2*i+1],sizeof(complex));
 	}
+
 	complex y0[N/2];
 	complex y1[N/2];
 	FFT(N/2,a0,y0);
-	FFT(N/2,a1,y1);		
-	for (i=0;i<N/2-1;++i){
-		y[k].val=y0[k].val+w.val*y1[k].val;
-		y[k].power=y0[k].power
-		
-		w.power+=2;
-		if (w.power==N){
-			w.val*=-1;
-			w.power=0;
-		}
+	FFT(N/2,a1,y1);	
+	
+	for (i=0;i<=N/2-1;++i){
+		complexmul(&tmp,w,y1[i]);		//tmp=w*y1[i]		
+		complexadd(&y[i],y0[i],tmp);		//y[i]=y0[i]+w*y1[i]
+
+		complexmul(&tmp,w,y1[i]);		//tmp=w*y1[i]
+		complexsub(&y[i+N/2],y0[i],tmp);	//y[i+N/2]=y0[i]-w*y1[i];		
+
+		complexmul(&w,w,wn);
 	}
 }
 
 void Multiply(int N){
 	int i;
 	for (i=0;i<N;++i){
-		yc[i]=complexmul(ya[i],yb[i]);
+		complexmul(&yc[i],ya[i],yb[i]);
 	}
 }
 
 void Reverse_FFT(int N,complex y[],complex a[]){
-	if (N==1) return;
+	if (N==1) {
+		a[0].real=y[0].real;
+		a[0].im=y[0].im;
+		return;
+	}
 	//initialize
 	int i;
-	complex w;
-	w.val=1;
-	w.power=0;
+	complex w,wn,tmp;
+	w.real=1;
+	w.im=0;
+	wn.real=cos(-2*M_PI/N);
+	wn.im=sin(-2*M_PI/N);
 	complex y0[N/2];
 	complex y1[N/2];
 	for (i=0;i<N/2;++i){
@@ -83,21 +99,17 @@ void Reverse_FFT(int N,complex y[],complex a[]){
 	complex a1[N/2];
 	Reverse_FFT(N/2,y0,a0);
 	Reverse_FFT(N/2,y1,a1);		
-	for (i=0;i<N/2-1;++i){
-		a[k].val=a0[k].val+w.val*a1[k].val;
-		a[k].power=a0[k].power
-		
-		w.power+=2;
-		if (w.power==N){
-			w.val*=-1;
-			w.power=0;
-		}
+	for (i=0;i<=N/2-1;++i){
+		complexmul(&tmp,w,a1[i]);
+		complexadd(&a[i],a0[i],tmp);
+		complexmul(&tmp,w,a1[i]);
+		complexsub(&a[i+N/2],a0[i],tmp);		
+		complexmul(&w,w,wn);
 	}
 }
 
 int main(){
 	int i,j,N,places,x,flag;
-	complex ya[64],yb[64];
 	int scale[4]={4,16,32,60};
 	FILE *fp;
 	fp=fopen("input.txt","r+");
@@ -111,7 +123,7 @@ int main(){
 	
 
 	fp=fopen("result.txt","w+");
-	fprintf("CALCULATION RESULT:\n");
+	fprintf(fp,"CALCULATION RESULT:\n");
 	printf("**************CALCULATION START...****************\n");
 	for (i=0;i<4;++i){
 
@@ -127,41 +139,44 @@ int main(){
 		if (flag){			//not 2'power
 			N=1<<(places+1);	//(int)lg(scale[i])+1
 			for (j=0;j<scale[i];++j){
-				aa[j].val=A[j];
-				aa[j].power=0;
-				ab[j].val=B[j];
-				ab[j].power=0;
+				aa[j].real=A[j];
+				aa[j].im=0;
+				ab[j].real=B[j];
+				ab[j].im=0;
 			}
-			for (j=scale[i];j<2N;++j){	//let A[scale[i]...2N] be 0  ;B[scale[i]...2N] be 0
-				aa[j].val=0;
-				aa[j].power=0;
-				ab[j].val=0;
-				ab[j].power=0;
+			for (j=scale[i];j<2*N;++j){	//let A[scale[i]...2N] be 0  ;B[scale[i]...2N] be 0
+				aa[j].real=0;
+				aa[j].im=0;
+				ab[j].real=0;
+				ab[j].im=0;
 			}
 			FFT(N,aa,ya);			//DFT(A)
 			FFT(N,ab,yb);			//DFT(B)
 			Multiply(2*N);			//DFT(A)*DFT(B)
-			Reverse_FFT(2*N,yc);		//C=REVERSE_DFT(DFT(A)*DFT(B))
+			Reverse_FFT(2*N,yc,ac);		//C=REVERSE_DFT(DFT(A)*DFT(B))
+			for (j=0;j<2*N;++j) C[j]=ac[j].real/(2*N);
 		}
 		else{
+			N=scale[i];
 			for (j=0;j<scale[i];++j){
-				ya[j].val=A[j];
-				ya[j].power=0;
-				yb[j].val=B[j];
-				yb[j].power=0;
+				aa[j].real=A[j];
+				aa[j].im=0;
+				ab[j].real=B[j];
+				ab[j].im=0;
 			}
 			for (j=scale[i];j<2*N;++j){	//let A[scale[i]...2N] be 0  ;B[scale[i]...2N] be 0
-				ya[j].val=0;
-				ya[j].power=0;
-				yb[j].val=0;
-				yb[j].power=0;
+				aa[j].real=0;
+				aa[j].im=0;
+				ab[j].real=0;
+				ab[j].im=0;
 			}
-			N=scale[i];
-			FFT(2*N,ya);
-			FFT(2*N,yb);
-			Multiply(2*N);
-			Reverse_FFT(2*N,yc);
+
+			FFT(2*N,aa,ya);			//DFT(A)
+			FFT(2*N,ab,yb);			//DFT(B)
+			Multiply(2*N);			//DFT(A)*DFT(B)
+			Reverse_FFT(2*N,yc,ac);		//C=REVERSE_DFT(DFT(A)*DFT(B))
 		}
+		for (j=0;j<2*N;++j) C[j]=ac[j].real/(2*N);
 		timeend();
 	
 		//print result to file
